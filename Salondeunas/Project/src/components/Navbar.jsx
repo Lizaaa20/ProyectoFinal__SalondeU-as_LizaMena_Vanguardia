154fa975904cc79645bfba2d8c9fcf8dadbe6ba5
Lizaaa20 <lizavalentinatorresmena@gmail.com>
2026-03-15 03:49:37 -0600

    Update Navbar.jsx

--- Salondeunas/Project/src/components/Navbar.jsx
+++ Salondeunas/Project/src/components/Navbar.jsx
@@ -1,63 +1,30 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
     return (
-        // Barra de navegación principal con fondo oscuro
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
+        // Barra de navegación principal
+        <nav className="navbar">
+            <div className="navbar-container">
 
-                {/* Logo / nombre del salón */}
-                <Link className="navbar-brand fw-bold" to="/">
-                     Salón de Uñas
+                {/* Logo del salón */}
+                <Link to="/" className="navbar-logo">
+                    ✦ Salón de Uñas
                 </Link>
 
-                {/* Botón para colapsar el menú en móviles */}
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                {/* Links de navegación */}
+                <ul className="navbar-links">
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/servicios">Servicios</Link></li>
+                    <li><Link to="/sucursales">Sucursales</Link></li>
+                    <li><Link to="/quienes-somos">Quiénes somos</Link></li>
+                </ul>
 
-                {/* Links del menú */}
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav me-auto">
+                {/* Botón iniciar sesión */}
+                <Link to="/login" className="navbar-login">
+                    Iniciar sesión
+                </Link>
 
-                        {/* Link a inicio */}
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Inicio</Link>
-                        </li>
-
-                        {/* Link al catálogo de servicios */}
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/servicios">Servicios</Link>
-                        </li>
-
-                        {/* Link a sucursales */}
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/sucursales">Sucursales</Link>
-                        </li>
-
-                        {/* Link a quiénes somos */}
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/quienes-somos">Quiénes Somos</Link>
-                        </li>
-
-                    </ul>
-
-                    {/* Botones de login y register */}
-                    <div className="d-flex gap-2">
-                        <Link className="btn btn-outline-light btn-sm" to="/login">
-                            Iniciar Sesión
-                        </Link>
-                        <Link className="btn btn-danger btn-sm" to="/register">
-                            Registrarse
-                        </Link>
-                    </div>
-
-                </div>
             </div>
         </nav>
     );
