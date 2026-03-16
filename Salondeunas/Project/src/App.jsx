9ab1377df469051223bf8af1356a4106dcba326b
Lizaaa20 <lizavalentinatorresmena@gmail.com>
2026-03-15 22:50:38 -0600

    Update App.jsx

--- Salondeunas/Project/src/App.jsx
+++ Salondeunas/Project/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import './App.css'
-
-
-  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Importar componentes
 import Navbar from './components/Navbar';
 
 // Importar páginas
 import Home from './pages/Home';
+
+
 
 function App() {
     return (
@@ -24,6 +24,8 @@ function App() {
             <Routes>
                 {/* Página de inicio */}
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
 
         </BrowserRouter>
