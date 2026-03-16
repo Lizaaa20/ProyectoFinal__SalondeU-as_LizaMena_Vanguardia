bf076a62e3ab5bddad4aa7f75be4bbef5c8abc63
Lizaaa20 <lizavalentinatorresmena@gmail.com>
2026-03-15 23:13:03 -0600

    Update App.jsx

--- Salondeunas/Project/src/App.jsx
+++ Salondeunas/Project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
+import Login from './auth/Login';
+import Register from './auth/Register';
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
