583cb0d66fda61d595775745ff4c682276ab1a1f
Lizaaa20 <lizavalentinatorresmena@gmail.com>
2026-03-17 03:04:33 -0600

    Update LoginAdmin.jsx

--- Salondeunas/Project/src/auth/LoginAdmin.jsx
+++ Salondeunas/Project/src/auth/LoginAdmin.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import logo from '../../assets/page_logo.png';
+import logo from '../assets/page_logo.png';
 import './Auth.css';
 
 function LoginAdmin() {
